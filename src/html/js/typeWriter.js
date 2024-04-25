fa86b3f48b86fbceb167a18311bab994becd520d
bdayceleb <168142277+bdayceleb@users.noreply.github.com>
2024-04-26 03:59:27 +0530

    Add files via upload

---
+++ src/html/js/typeWriter.js
@@ -0,0 +1,37 @@
+
+let i = 0;
+let text1 = "Hey!! Hottie.. Today is the Big Day.";
+let text2 = "On this day, the universe witnessed the fulfillment of your existence, as the divine bestowed upon me the greatest gift—your presence in my life, blessing every moment with beauty and love beyond compare."
+let text3 = ""
+let speed = 100;
+
+function typeWriter(text, para){
+	if(ok == 2){
+		clearInterval(typeInterval);
+	}
+	if(i < text.length){
+		document.getElementById(para).innerHTML += text.charAt(i);
+		i++;
+		speed = Math.random() * 50 + 100;
+	}
+	else{
+		if(ok == 0){
+			i = 0;
+		}
+		ok += 1;
+	}
+}
+
+var typeInterval;
+
+//window.onload = function() {
+//	window.onload = function(){};
+   	typeInterval = setInterval(function(){
+		if(ok == 0){
+			typeWriter(text1, "txt1");
+		}
+		else if(ok == 1){
+			typeWriter(text2, "txt2");
+		}
+	}, 100);
+//};
